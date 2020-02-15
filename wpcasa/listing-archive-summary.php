28d7c4ae816bc3db7cc2594ec037147fefcbf3ee
Igor Chernenko <igorchernenko92@gmail.com>
2020-02-15 15:56:39 +0200

    ADD: archive changes

--- wpcasa/listing-archive-summary.php
+++ wpcasa/listing-archive-summary.php
@@ -2,7 +2,7 @@
 
 	<?php do_action( 'wpsight_listing_archive_summary_before' ); ?>
 
-	<?php wpsight_listing_summary(get_the_ID(), array('details_4', 'details_3')); ?>
+	<?php wpsight_listing_summary(get_the_ID(), array('details_3', 'details_4')); ?>
 
     <?php wpsight_listing_price(); ?>
 
